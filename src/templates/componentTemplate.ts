c83902a56f48d74c48d140c3498d607e8425385b
Alexandre Hideki Siroma <alexandresiroma@hotmail.com>
2023-03-30 23:34:02 -0300

    fix(Template): Fix component template with stitchesjs

--- src/templates/componentTemplate.ts
+++ src/templates/componentTemplate.ts
@@ -3,40 +3,40 @@ export const componentTemplate = (params: { componentName: string, usingStitches
 
   if (usingStitchesJs) {
     return (
-      `import React, { ReactNode } from "react"; \n` +
-      `// import { styled } from "@config/stitches.config"; \n` +
+      `import React, { ReactNode } from "react";\n` +
+      `// import { styled } from "@config/stitches.config";\n` +
       `\n` +
-      `import { ${componentName}ContainerCSS, Styled${componentName}Container } from "./${componentName}.styles"; \n` +
+      `import { ${componentName}ContainerCSS, Styled${componentName}Container } from "./${componentName}.styles";\n` +
       `\n` +
-      `// const Styled${componentName}Container = styled("div", ${componentName}ContainerCSS); \n` +
+      `// const Styled${componentName}Container = styled("div", ${componentName}ContainerCSS);\n` +
       `\n` +
-      `export type ${componentName}Props = { \n` +
-      `\tchildren: ReactNode;  \n` +
+      `export type ${componentName}Props = {\n` +
+      `\tchildren: ReactNode;\n` +
       `}\n` +
       `\n` +
-      `export const ${componentName} = ({ children, ...props }: ${componentName}Props) => { \n` +
-      `\treturn ( \n` +
-      `\t\t<${componentName}Container> \n` +
-      `\t\t\t{children} \n` +
-      `\t\t</${componentName}Container> \n` +
-      `\t) \n` +
+      `export const ${componentName} = ({ children, ...props }: ${componentName}Props) => {\n` +
+      `\treturn (\n` +
+      `\t\t<Styled${componentName}Container>\n` +
+      `\t\t\t{children}\n` +
+      `\t\t</Styled${componentName}Container>\n` +
+      `\t)\n` +
       `}\n`
     );
   }
 
   return (
-    `import React, { ReactNode } from "react"; \n` +
+    `import React, { ReactNode } from "react";\n` +
     `\n` +
-    `export type ${componentName}Props = { \n` +
-    `\tchildren: ReactNode;  \n` +
+    `export type ${componentName}Props = {\n` +
+    `\tchildren: ReactNode; \n` +
     `}\n` +
     `\n` +
-    `export const ${componentName} = ({ children }: ${componentName}Props) => { \n` +
-    `\treturn ( \n` +
-    `\t\t<> \n` +
-    `\t\t\t{children} \n` +
-    `\t\t</> \n` +
-    `\t) \n` +
+    `export const ${componentName} = ({ children }: ${componentName}Props) => {\n` +
+    `\treturn (\n` +
+    `\t\t<>\n` +
+    `\t\t\t{children}\n` +
+    `\t\t</>\n` +
+    `\t)\n` +
     `}\n`
   );
 };
