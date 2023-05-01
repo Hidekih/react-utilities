da5d13519e5b97e2b28173244626f73aae3ba1e0
Alexandre Hideki Siroma <alexandresiroma@hotmail.com>
2023-05-01 14:37:50 -0300

    Uptade break line to CRLF

--- src/templates/indexTemplate.ts
+++ src/templates/indexTemplate.ts
@@ -1,6 +1,6 @@
 export const indexTemplate = (componentName: string) => {
   return (
-    `export { ${componentName} } from "./${componentName}"; \n` +
-    `export type { ${componentName}Props } from "./${componentName}"; \n`
+    `export { ${componentName} } from "./${componentName}"; \r\f` +
+    `export type { ${componentName}Props } from "./${componentName}"; \r\f`
   );
 };
